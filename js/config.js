e160e03db506b8ed84fa553309ea867bdb8b596b
Nash1610 <140499120+Nash1610@users.noreply.github.com>
2025-11-09 20:03:27 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_5Caulb8kK',
-        userPoolClientId: '5peinefhkhr0o44du08etqar99',
+        userPoolClientId: '6cp7veueqcaj87gui2mk79unq3',
         region: 'us-east-1'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
